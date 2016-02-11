3e7d93408248d16d2d92e21eed08e6a9aee23d91
Miguel Ángel Grau Martínez <elgrau@Mac-mini-de-Miguel.local>
2016-02-12 00:30:38 +0100

    Adds new login template

--- public/js/views/auth/login.js
+++ public/js/views/auth/login.js
@@ -9,6 +9,7 @@ define(['jquery', 'underscore', 'backbone', 'models/users/user', 'text!templates
     initialize: function() {
       $('body').addClass('loginBackground');
       this.$el.html(_.template(loginTemplate, {}));
+      this.loadBackgroundImages();
     },
     reset: function(e) {
       e.preventDefault();
@@ -44,6 +45,81 @@ define(['jquery', 'underscore', 'backbone', 'models/users/user', 'text!templates
           }
         }
       });
+    },
+    loadBackgroundImages: function() {
+      // canUse
+      window.canUse = function(p) {
+        if (!window._canUse) window._canUse = document.createElement("div");
+        var e = window._canUse.style,
+          up = p.charAt(0).toUpperCase() + p.slice(1);
+        return p in e || "Moz" + up in e || "Webkit" + up in e || "O" + up in e || "ms" + up in e
+      };
+
+      // Settings.
+      var settings = {
+
+        // Images (in the format of 'url': 'alignment').
+        images: {
+          'style/images/bg01.jpg': 'center',
+          'style/images/bg02.jpg': 'center',
+          'style/images/bg03.jpg': 'center'
+        },
+
+        // Delay.
+        delay: 6000
+      };
+
+      // Vars.
+      var pos = 0,
+        lastPos = 0,
+        bgs = [],
+        k, v;
+
+      // Create BG wrapper, BGs.
+      $('body').append('<div id="bg"></div>');
+
+      for (k in settings.images) {
+
+        // Create BG.
+        var bg = $('<div></div>').css({
+          'backgroundImage': 'url("' + k + '")',
+          'backgroundPosition': settings.images[k]
+        });
+        bg.appendTo('#bg')
+
+        // Add it to array.
+        bgs.push(bg);
+
+      }
+
+      // Main loop.
+      bgs[pos].addClass('visible');
+      bgs[pos].addClass('top');
+
+      // Bail if we only have a single BG or the client doesn't support transitions.
+      if (bgs.length == 1 || !canUse('transition'))
+        return;
+
+      window.setInterval(function() {
+
+        lastPos = pos;
+        pos++;
+
+        // Wrap to beginning if necessary.
+        if (pos >= bgs.length)
+          pos = 0;
+
+        // Swap top images.
+        bgs[lastPos].removeClass('top');
+        bgs[pos].addClass('visible');
+        bgs[pos].addClass('top');
+
+        // Hide last image after a short delay.
+        window.setTimeout(function() {
+          bgs[lastPos].removeClass('visible');
+        }, settings.delay / 2);
+
+      }, settings.delay);
     }
   });
   return LoginView;
